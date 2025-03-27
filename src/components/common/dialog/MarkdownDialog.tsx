a3d17aa66a7f8c809c5cde1f83cc57f99f433564
blendy9702 <lch99206@naver.com>
2025-03-27 10:15:07 +0900

    read

--- src/components/common/dialog/MarkdownDialog.tsx
+++ src/components/common/dialog/MarkdownDialog.tsx
@@ -1,10 +1,14 @@
 "use client";
+
+import { useState } from "react";
 // SCSS
 import styles from "@/components/common/dialog/MarkdownDialog.module.scss";
-import { Checkbox } from "@/components/ui/checkbox";
+
 // Markdown
 import MDEditor from "@uiw/react-md-editor";
+
 // shadcn/ui
+import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -15,14 +19,16 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Separator } from "@/components/ui/separator";
-import LabelCalendar from "../calendar/LabelCalendar";
-import { useState } from "react";
 import { toast } from "sonner";
+
+// 컴포넌트
+import LabelCalendar from "../calendar/LabelCalendar";
 import { createTodo } from "@/app/actions/todos-action";
 
 function MarkdownDialog() {
   // 다이얼로그 Props
   const [open, setOpen] = useState<boolean>(false);
+
   // 에디터의 제목/본문 내용
   const [title, setTitle] = useState<string | undefined>("");
   const [content, setContent] = useState<string | undefined>("");
@@ -30,28 +36,29 @@ function MarkdownDialog() {
   // todo 작성
   const onSubmit = async () => {
     if (!title || !content) {
-      toast.error("부정적인! 입력하다 항목.", {
-        description: "입력하다 제목 또는 내용",
+      toast.error("입력항목을 확인해 주세요.", {
+        description: "제목과 내용을 입력해주세요.",
         duration: 3000,
       });
       return;
     }
 
-    // 서버 액션 실행하기
+    // 서버액션 실행하기
     const { data, error, status } = await createTodo({
-      content,
-      title,
+      content: content,
+      title: title,
     });
+
     if (error) {
-      toast.error("실패하다 등록 :(", {
+      toast.error("등록 실패.", {
         description: `Error ${error.message}`,
         duration: 3000,
       });
       return;
     }
 
-    toast.success("긍정적인!", {
-      description: "등록되다 글이 supabase",
+    toast.success("성공하였습니다.", {
+      description: "Supabase에 글이 등록되었습니다.",
       duration: 3000,
     });
 
@@ -64,7 +71,7 @@ function MarkdownDialog() {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <span className="flex justify-center w-full font-normal text-gray-400 hover:text-gray-500 cursor-pointer">
+        <span className="w-full justify-center flex font-normal text-gray-400 hover:text-gray-500 cursor-pointer">
           Add Content
         </span>
       </DialogTrigger>
@@ -89,7 +96,7 @@ function MarkdownDialog() {
           <Separator />
           {/* 마크다운 입력 영역 */}
           <div className={styles.dialog_markdown}>
-            <MDEditor height={"80%"} value={content} onChange={setContent} />
+            <MDEditor height={"100%"} value={content} onChange={setContent} />
           </div>
         </DialogHeader>
         <DialogFooter>
